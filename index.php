877e806701e7f89efda18a1f6886ddd61676c396
Fausto Ruiz Madrid <faustoruizmadrid@gmail.com>
2022-04-17 23:26:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,22 @@ require_once('twitter-api-php/TwitterAPIExchange.php');
 require 'vendor/autoload.php';
 use Cvuorinen\Raspicam\Raspistill;
 
+/*
+cuenta antigua
 $settings = array(
     'oauth_access_token' => "2929730890-FzwuPzlQ628VZlehaJXPZexEQEKyOvjG26OEgvG",
     'oauth_access_token_secret' => "7CMdLTuRMwOJ8EFLUkK17c0YP1gGCX26HtyQMcLdU6mmZ",
     'consumer_key' => "ExYUACK1zPYudi1cEoHmk1Mwz",
     'consumer_secret' => "IGJ6tHRVkLZd8vP0xhEyynV5hHp6HVRc9aw66S4XYBmgcRjNQh"
+);*/
+$settings = array(
+    'oauth_access_token' => "WEVTSF9yMjVSN3E1dUFzczRvd1o6MTpjaQ",
+    'oauth_access_token_secret' => "Xca2MTzVlu0eelGKfmicaw2r8R4U17MDBlyYuZH_kOqnkohAVy",
+    'consumer_key' => "b75xPpzpsdnjf08Yo340mHvbF",
+    'consumer_secret' => "N0zOqUzGePVHEu3pJ1OMs1XqWVdhG2bn0lzaaYzpziNQmZXKZb"
 );
 
-passthru("raspistill -o pics/test-jpg");
+passthru("raspistill -o pics/test.jpg");
 
 $camera = new Raspistill();
 $camera->timeout(1)
@@ -60,7 +68,7 @@ $url = 'https://api.twitter.com/1.1/statuses/update.json';
 $requestMethod = 'POST';
 $postfields = array(
 	'media_ids' => $id,
-	'status' => 'Hola holis' );
+	'status' => 'Foto' );
 
 if(strlen($postfields['status']) <= 140)
 {
